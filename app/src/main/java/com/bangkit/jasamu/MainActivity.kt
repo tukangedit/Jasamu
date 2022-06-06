6081fd2534eb21647d8fdc643998984070564920
tukangedit <taufiknur086@gmail.com>
2022-06-07 07:21:06 +0800

    colors.xml MainActivity.kt

--- app/src/main/java/com/bangkit/jasamu/MainActivity.kt
+++ app/src/main/java/com/bangkit/jasamu/MainActivity.kt
@@ -2,26 +2,31 @@ package com.bangkit.jasamu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.bangkit.jasamu.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.NavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val appBarConfiguration = AppBarConfiguration.Builder(
             R.id.berandaFragment, R.id.jualFragment, R.id.riwayatFragment, R.id.profilFragment
         ).build()
-
-        val navHostFragment =  supportFragmentManager.findFragmentById(R.id.main_Container) as NavHostFragment
+        val navHostFragment =  supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFragment.navController
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
         setupWithNavController(bottomNavigationView, navController)
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
